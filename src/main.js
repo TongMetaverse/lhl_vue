9afa7cd411e745b7c2011e474eac4dabd87ed89a
zhaotong <1427730623@qq.com>
2021-12-02 01:06:04 +0800

    项目初始化

--- src/main.js
+++ src/main.js
@@ -2,11 +2,24 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import * as echarts from 'echarts';
+import 'echarts-liquidfill'
+
+import VChart from "vue-echarts"
+
+
+Vue.component('v-chart', VChart)
+
+Vue.use(ElementUI);
 
 Vue.config.productionTip = false
 
-new Vue({
+const app = new Vue({
   router,
   store,
   render: function (h) { return h(App) }
 }).$mount('#app')
+
+
