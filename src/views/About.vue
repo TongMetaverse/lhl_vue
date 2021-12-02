e794922e1f4056b9d03614b2bfeb37a9d293159b
zhaotong_win11 <1427730623@qq.com>
2021-12-02 18:13:56 +0800

    提交

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,85 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <el-row>
+  <el-button>默认按钮</el-button>
+  <el-button type="primary">主要按钮</el-button>
+  <el-button type="success">成功按钮</el-button>
+  <el-button type="info">信息按钮</el-button>
+  <el-button type="warning">警告按钮</el-button>
+  <el-button type="danger">危险按钮</el-button>
+</el-row>
+
+    <v-chart class="chart" :option="option" />
+    <router-view />
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      option: {
+        title: {
+          text: "Traffic Sources",
+          left: "center",
+        },
+        tooltip: {
+          trigger: "item",
+          formatter: "{a} <br/>{b} : {c} ({d}%)",
+        },
+        legend: {
+          orient: "vertical",
+          left: "left",
+          data: [
+            "Direct",
+            "Email",
+            "Ad Networks",
+            "Video Ads",
+            "Search Engines",
+          ],
+        },
+        series: [
+          {
+            name: "Traffic Sources",
+            type: "pie",
+            radius: "55%",
+            center: ["50%", "60%"],
+            data: [
+              { value: 335, name: "Direct" },
+              { value: 310, name: "Email" },
+              { value: 234, name: "Ad Networks" },
+              { value: 135, name: "Video Ads" },
+              { value: 1548, name: "Search Engines" },
+            ],
+            emphasis: {
+              itemStyle: {
+                shadowBlur: 10,
+                shadowOffsetX: 0,
+                shadowColor: "rgba(0, 0, 0, 0.5)",
+              },
+            },
+          },
+        ],
+      },
+    };
+  },
+};
+</script>
+
+<style lang="stylus">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.chart {
+  height: 400px;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,40 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="main">
+    <el-menu
+      :default-active="activeIndex"
+      class="nav"
+      mode="horizontal"
+      @select="handleSelect"
+      background-color="#112D60"
+    >
+      <el-row type="flex" justify="space-between">
+        <el-col style="width: 200px"
+          ><el-menu-item index="1"
+            ><span class="title">接口管理中心</span></el-menu-item
+          ></el-col
+        >
+        <el-col style="width: 200px"
+          >
+          <span><i class="el-icon-s-custom" s/>admin</span>
+          </el-col
+        >
+      </el-row>
+    </el-menu>
+    <router-view />
   </div>
 </template>
-
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      activeIndex: 0,
+    };
+  },
+};
 </script>
+<style scoped>
+.nav .title {
+  font-size: 24px;
+  color: rgba(255, 255, 255, 0.8);
+}
+</style>
