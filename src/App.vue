036c589b68be91fa9297ddfcbb338a4d160e5292
zhaotong_win11 <1427730623@qq.com>
2021-12-02 18:14:04 +0800

    提交

--- src/App.vue
+++ src/App.vue
@@ -1,76 +1,14 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <v-chart class="chart" :option="option" />
     <router-view />
   </div>
 </template>
-<script>
-export default {
-  data() {
-    return {
-      option: {
-        title: {
-          text: "Traffic Sources",
-          left: "center",
-        },
-        tooltip: {
-          trigger: "item",
-          formatter: "{a} <br/>{b} : {c} ({d}%)",
-        },
-        legend: {
-          orient: "vertical",
-          left: "left",
-          data: [
-            "Direct",
-            "Email",
-            "Ad Networks",
-            "Video Ads",
-            "Search Engines",
-          ],
-        },
-        series: [
-          {
-            name: "Traffic Sources",
-            type: "pie",
-            radius: "55%",
-            center: ["50%", "60%"],
-            data: [
-              { value: 335, name: "Direct" },
-              { value: 310, name: "Email" },
-              { value: 234, name: "Ad Networks" },
-              { value: 135, name: "Video Ads" },
-              { value: 1548, name: "Search Engines" },
-            ],
-            emphasis: {
-              itemStyle: {
-                shadowBlur: 10,
-                shadowOffsetX: 0,
-                shadowColor: "rgba(0, 0, 0, 0.5)",
-              },
-            },
-          },
-        ],
-      },
-    };
-  },
-};
-</script>
-
-<style lang="stylus">
+<style scoped>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-.chart {
-  height: 400px;
+  width: 100%;
+  height: 100%;
+  padding: 0;
+  margin: 0;
 }
 </style>
---
+++ src/views/Login.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="main">
+    <div class="content">
+      <span>接口管理平台</span>
+      <el-form class="form" ref="form" :model="form" label-width="80px">
+        <el-form-item label="用户名">
+          <el-input v-model="form.name"></el-input>
+        </el-form-item>
+        <el-form-item label="密码">
+          <el-input v-model="form.pwd"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <router-link to="/home"><el-button style="width:100%;border-radius: 28px;" type="info" >登录</el-button></router-link>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        name: "",
+        pwd: "",
+      },
+    };
+  },
+};
+</script>
+<style  scoped>
+.main {
+  width: 100%;
+  height: 100vh;
+  overflow: hidden;
+  background: url("../assets/imgs/bg@2x.png") no-repeat;
+  background-size: 100% 100%;
+  background-position: center center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 15vh;
+}
+span {
+  width: 100%;
+  height: 73px;
+  display: block;
+  overflow-wrap: break-word;
+  color: rgba(121, 230, 255, 1);
+  font-size: 32px;
+  white-space: nowrap;
+  line-height: 73px;
+  text-align: center;
+  align-self: center;
+}
+.form {
+  margin-top: 10vh;
+  margin-left: -50px;
+}
+
+.form >>> .el-input__inner {
+  border-radius: 28px;
+  background-color: rgba(121, 230, 255, 0.3);
+  width: 300px;
+  color: rgba(255, 255, 255, 0.8);
+}
+
+.form >>> .el-form-item__label {
+  color: rgba(255, 255, 255, 0.8);
+}
+</style>
